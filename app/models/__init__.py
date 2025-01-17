a7d07f3240d9018d875481866fdb3862502529a5
Herisson Nobre <herisson.carvalho96@gmail.com>
2025-01-17 14:13:30 -0300

    refactor: turns models to a modular solution

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+"""
+Init file
+"""
+from .user import User
+from .task import Task
--- app/models/task.py
+++ app/models/task.py
@@ -1,33 +1,16 @@
 """
-Database models for users and tasks.
+Task model for the database.
 
 Classes:
-    - User: Represents a user.
     - Task: Represents a task.
 """
 import uuid
 from datetime import datetime
 
 from sqlalchemy import UUID
-from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.extensions import db
-
-
-class User(db.Model):
-    """
-    User model.
-    """
-    __tablename__ = 'users'
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email: Mapped[str] = mapped_column(unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
-
-    tasks: Mapped[list['Task']] = relationship('Task', back_populates='user')
-
-    def __repr__(self):
-        return f'<User {self.email}>'
 
 
 class Task(db.Model):
